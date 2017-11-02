f6baf3ff558b29e87d303a1404b4f96ba1f6e0af
Daniel Gerdes <daniel.gerdes.hagen@gmail.com>
2017-11-02 17:05:45 +0100

    UI really ready

---
+++ PWGen UI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PWGen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mypws_Click(object sender, RoutedEventArgs e)
+        {
+            PasswoerterPanel.Visibility = Visibility.Visible;
+            ErstellenPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void create_Click(object sender, RoutedEventArgs e)
+        {
+            ErstellenPanel.Visibility = Visibility.Visible;
+            PasswoerterPanel.Visibility = Visibility.Hidden;
+        }
+    }
+}
